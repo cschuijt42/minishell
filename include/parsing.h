61e20a4cc386651577a958ba290cbae2f18c86a0
Casper Schuijt <casperschuijt@gmail.com>
2023-02-27 01:58:49 +0100

    Structs and enums

---
+++ include/parsing.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   parsing.h                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: cschuijt <cschuijt@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/02/26 22:43:08 by cschuijt      #+#    #+#                 */
+/*   Updated: 2023/02/26 22:43:08 by cschuijt      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSING_H
+# define PARSING_H
+
+typedef enum e_redirect_types {
+	redirect_input,
+	redirect_heredoc,
+	redirect_output,
+	redirect_output_append
+}	t_redirect_types;
+
+typedef struct s_command {
+	char				*target;
+	char				*target_expanded;
+	struct s_argument	*arguments;
+	struct s_redirect	*redirects;
+	struct s_command	*next;
+	struct s_command	*prev;
+}	t_command;
+
+typedef struct s_argument {
+	char				*content;
+	struct s_argument	*next;
+}	t_argument;
+
+typedef struct s_redirect {
+	char				*target;
+	char				*target_expanded;
+	int					type;
+	struct s_redirect	*next;
+}	t_redirect;
+
+#endif
---
+++ sources/parsing.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   parsing.c                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: cschuijt <cschuijt@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/02/26 22:42:52 by cschuijt      #+#    #+#                 */
+/*   Updated: 2023/02/26 22:42:52 by cschuijt      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <lexing.h>
+#include <parsing.h>
+
+t_command	build_command_tree(t_lexnode *input)
+{
+}
