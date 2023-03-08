1d782ce2796a9f15928ee3199379d0963aa7c2dc
Casper Schuijt <casperschuijt@gmail.com>
2023-03-08 01:19:01 +0100

    Add $? lexing

--- sources/lexing_expanding.c
+++ sources/lexing_expanding.c
@@ -22,11 +22,17 @@ void	expand_variable_in_quotes(char *var_name, t_shell *shell)
 {
 	char	*var_value;
 
-	var_value = get_env_var_value(var_name, shell->environment);
-	if (!var_value)
-		return ;
-	add_nested_node_to_lexer_output(ft_strdup(var_value), \
-									token_plain_text, shell);
+	if (var_name[0] == '?')
+		add_nested_node_to_lexer_output(ft_itoa(g_return_value), \
+										token_plain_text, shell);
+	else
+	{
+		var_value = get_env_var_value(var_name, shell->environment);
+		if (!var_value)
+			return ;
+		add_nested_node_to_lexer_output(ft_strdup(var_value), \
+										token_plain_text, shell);
+	}
 }
 
 void	expand_variable(char *var_name, t_shell *shell, int i)
@@ -34,18 +40,24 @@ void	expand_variable(char *var_name, t_shell *shell, int i)
 	char	*var_value;
 	int		start;
 
-	var_value = get_env_var_value(var_name, shell->environment);
-	if (!var_value)
-		return ;
-	start = i;
-	while (var_value[i] && !is_whitespace(var_value[i]))
-		i++;
-	add_nested_node_to_lexer_output(ft_substr(var_value, start, i - start), \
-									token_plain_text, shell);
-	i = skip_whitespace(var_value, i);
-	if (var_value[i])
+	if (var_name[0] == '?')
+		add_nested_node_to_lexer_output(ft_itoa(g_return_value), \
+										token_plain_text, shell);
+	else
 	{
-		add_node_to_lexer_output(ft_strdup(""), token_plain_text, shell);
-		expand_variable(var_name, shell, i);
+		var_value = get_env_var_value(var_name, shell->environment);
+		if (!var_value)
+			return ;
+		start = i;
+		while (var_value[i] && !is_whitespace(var_value[i]))
+			i++;
+		add_nested_node_to_lexer_output(ft_substr(var_value, start, i - start), \
+										token_plain_text, shell);
+		i = skip_whitespace(var_value, i);
+		if (var_value[i])
+		{
+			add_node_to_lexer_output(ft_strdup(""), token_plain_text, shell);
+			expand_variable(var_name, shell, i);
+		}
 	}
 }
