7ecfd50f86a129da2dfb9f1d07b3a51daf77ebf9
Casper Schuijt <cschuijt@student.codam.nl>
2023-03-20 17:01:04 +0100

    Draft execution

---
+++ include/execution.h
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   execution.h                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: cschuijt <cschuijt@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/03/20 15:44:22 by cschuijt      #+#    #+#                 */
+/*   Updated: 2023/03/20 15:44:22 by cschuijt      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EXECUTION_H
+# define EXECUTION_H
+# include "minishell.h"
+
+#endif
