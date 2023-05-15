16e05b245ba14b9d727a56aac80db31bbf58d7fb
Casper Schuijt <casperschuijt@gmail.com>
2023-05-15 20:48:50 +0200

    Fix broken checks in libft

--- LIBFT/sources/mem/ft_memcpy.c
+++ LIBFT/sources/mem/ft_memcpy.c
@@ -17,12 +17,12 @@
 // matter because we cast the whole pointer to an unsigned char pointer and
 // therefore we only change the bits that belong to that part of memory
 //IE: if it's an Int array it would iterate 4 times for every int it changes
-void	*ft_memcpy(void	*dest, const void	*src, size_t	n)
+void	*ft_memcpy(void	*dest, const void *src, size_t n)
 {
 	size_t		i;
 
 	i = 0;
-	if (!dest && !src)
+	if (!dest || !src)
 		return (NULL);
 	while (i < n)
 	{
--- LIBFT/sources/mem/ft_memmove.c
+++ LIBFT/sources/mem/ft_memmove.c
@@ -1,23 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mde-cloe <mde-cloe@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/13 18:00:40 by mde-cloe          #+#    #+#             */
-/*   Updated: 2021/12/13 18:05:23 by mde-cloe         ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   ft_memmove.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mde-cloe <mde-cloe@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/12/13 18:00:40 by mde-cloe      #+#    #+#                 */
+/*   Updated: 2021/12/13 18:05:23 by mde-cloe      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void	*dst, const void	*src, size_t	len)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	size_t	i;
 
-	i = 0;
-	if (!dst && !src)
+	if (!dst || !src)
 		return (NULL);
 	if ((size_t)dst - (size_t)src < len)
 	{
